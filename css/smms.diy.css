f8b46776a217557df74b8e8dcdfbaee4c59c78b8
qcgzxw <qcgzxw@qq.com>
2018-02-27 15:57:07 +0800

    默认样式
    默认样式

--- css/smms.diy.css
+++ css/smms.diy.css
@@ -1,10 +1,10 @@
 /*
 Plugin Name: SM.MS图床外链小工具
 Plugin URI: https://www.qcgzxw.cn/2555.html
-Description: 小文博客独自开发的图床插件，用于WordPress博客添加图床上传小工具。
+Description: 小文博客独自开发的图床插件，用于WordPress博客添加 图床上传小工具、评论处图片上传按钮、文章编辑处图片上传按钮。
 Author: 小文博客
 Author URI: https://www.qcgzxw.cn/
-Version: 3.0
+Version: 4.0
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 自定义小工具CSS样式部分
 */
