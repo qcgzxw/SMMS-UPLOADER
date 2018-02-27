161dd90fb5d425dc63fbfaa4f17a323b347998ce
qcgzxw <qcgzxw@qq.com>
2018-02-27 15:59:11 +0800

    Ver4.0
    Ver4.0

--- SMMS-UPLOADER-COMMENTS.php
+++ SMMS-UPLOADER-COMMENTS.php
@@ -1,5 +1,7 @@
 <?php
-function zsky_upload_img() {
+function comment_upload_img() {
 echo '<div class="zz-add-img"><input id="zz-img-file" type="file" accept="image/*" multiple="multiple"><div id="zz-img-add">上传图片</div><div id="zz-img-show"></div></div>';
 }
-//add_filter('comment_form_after', 'zsky_upload_img');
+function admin_upload_img() {
+    echo '<a href="javascript:;" class="file">选择文件<input id="admin-img-file" type="file" accept="image/*" multiple="multiple"></a>';
+}
--- SMMS-UPLOADER-WIDGETS.php
+++ SMMS-UPLOADER-WIDGETS.php
@@ -37,12 +37,15 @@ class qcgzxw extends WP_Widget {
 		echo $result;
 		?>
 			<div class="upload_box" style = "padding: 20px 0 20px 10px">
-			<input type="file" id="image" accept="image/*" multiple="multiple" >
+			<a href="javascript:;" class="smms-upload-file">选择文件<input type="file" id="image" accept="image/*" multiple="multiple" ></a>
 				<div id = "show" style="widht:100%;height:100%;word-wrap: break-word">
 				<p class = "text-justify" id="urls"></p>
 					<div id = "mul-image"></div>
 				</div>
 			</div>
+			<style>.smms-upload-file{position:relative;display:inline-block;background:#D0EEFF;border:1px solid #99D3F5;border-radius:4px;padding:4px 12px;overflow:hidden;color:#1E88C7;text-decoration:none;text-indent:0;line-height:20px}
+.smms-upload-file input{position:absolute;font-size:100px;right:0;top:0;opacity:0}
+.smms-upload-file:hover{background:#AADFFD;border-color:#78C3F3;color:#004974;text-decoration:none}</style>
 		<?php
 		echo $after_widget;
     }
