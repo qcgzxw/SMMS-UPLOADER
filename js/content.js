37d938120fdd36d6aa4fa70a2709e87eb49e3cf6
qcgzxw <qcgzxw@qq.com>
2018-03-19 16:04:05 +0800

    Ver 4.2
    文章编辑处图片插入至光标

--- js/content.js
+++ js/content.js
@@ -11,10 +11,36 @@ jQuery(document).ready(function($) {
 				contentType: false,
 				data: formData,
 				success: function(res) {
-					$('textarea[name="content"]').val($('textarea[name="content"]').val() + '<img class="aligncenter" src="' + res.data.url + '" />').focus();
-					$("html").find("iframe").contents().find("body").append('<img class="aligncenter" src="' + res.data.url + '" />');
+					$('textarea[name="content"]').insertAtCaret('<img class="aligncenter" src="' + res.data.url + '" />');
+					$("html").find("iframe").contents().find("body").append('<img class="aligncenter" src="'+res.data.url+'" />'); 
 				}
 			})
 		}
 	});
+	$.fn.extend({  
+        insertAtCaret: function(myValue) {  
+            var $t = $(this)[0];  
+              //IE  
+            if (document.selection) {  
+                this.focus();  
+                sel = document.selection.createRange();  
+                sel.text = myValue;  
+                this.focus();  
+            } else  
+            //!IE  
+            if ($t.selectionStart || $t.selectionStart == "0") {  
+                var startPos = $t.selectionStart;  
+                var endPos = $t.selectionEnd;  
+                var scrollTop = $t.scrollTop;  
+                $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);  
+                this.focus();  
+                $t.selectionStart = startPos + myValue.length;  
+                $t.selectionEnd = startPos + myValue.length;  
+                $t.scrollTop = scrollTop;  
+            } else {  
+                this.value += myValue;  
+                this.focus();  
+            }  
+        }  
+    });
 })
